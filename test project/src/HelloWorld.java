7c7075ae4be1c0fb3f8c01b41ede5585f0965967
Anil <anil.kumar@aitglobalinc.com>
2020-01-07 19:44:08 +0530

    done for samp0le program

--- test project/src/HelloWorld.java
+++ test project/src/HelloWorld.java
@@ -2,5 +2,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("This is my test project");
+		int i=0;
+		i = i+5;
+		System.out.println("Value after update"+i);
 	}
 }
